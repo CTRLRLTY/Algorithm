9c700369265ffff69b7f2a99ce09b2c6b5d8e01f
Raznan <muhammadraznan@satpaminternet.com>
2021-10-31 20:43:08 +0700

    Seperated Process class

--- FCFS.cxx
+++ FCFS.cxx
@@ -1,38 +1,7 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
-
-// Process structure
-class Process {
-  float burst_time;
-  float arrival_time;
-
-  float execution_time{0.0f};
-
-public:
-  float start_time{0.0f};
-
-  Process(float p_burst_time, float p_arrival_time)
-      : burst_time{p_burst_time}, arrival_time{p_arrival_time} {}
-
-  float get_burst_time() const { return burst_time; }
-
-  float get_arrival_time() const { return arrival_time; }
-
-  float get_execution_time() const { return execution_time; }
-
-  float get_completion_time() const {
-    return execution_time + arrival_time + get_wait_time();
-  }
-
-  float get_turnaround_time() const {
-    return get_completion_time() - arrival_time;
-  }
-
-  float get_wait_time() const { return start_time - arrival_time; }
-
-  void execute() { ++execution_time; }
-};
+#include "Process.cxx"
 
 float get_average(const std::vector<float> &v) {
   float acc{0.0f};
